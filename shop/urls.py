62a0841c5dd10366c49935ad27240920e68ad0f8
semen-ksv <ksvsem@gmail.com>
2020-07-09 22:01:20 +0300

    7--removing and adding books in cart

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomeView, BookDetailView, OrderView, checkout, add_to_cart, remove_from_cart
+from .views import HomeView, BookDetailView, OrderView,\
+    checkout, add_to_cart, remove_from_cart, remove_book_in_cart, add_in_cart
 
 app_name = 'shop'
 
@@ -7,8 +8,11 @@ urlpatterns = [
     path('', HomeView.as_view(), name='books-list'),
     path('book/<str:slug>/', BookDetailView.as_view(), name='book'),
     path('checkout/', checkout, name='checkout'),
+    path('cart/', OrderView.as_view(), name='cart'),
     path('add_to_cart/<slug>', add_to_cart, name='add_to_cart'),
+    path('add_in_cart/<slug>', add_in_cart, name='add_in_cart'),
     path('remove_from_cart/<slug>', remove_from_cart, name='remove_from_cart'),
-    path('order/', OrderView.as_view(), name='order_summary'),
+    path('remove_book/<slug>', remove_book_in_cart, name='remove_book_in_cart'),
+
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -31,9 +31,9 @@ class OrderView(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
         try:
             order = Order.objects.get(user=self.request.user, ordered=False)
-            return render(self.request, "order_summary.html", {'order': order})
+            return render(self.request, "cart.html", {'order': order})
         except ObjectDoesNotExist:
-            messages.error(self.request, 'You dont have order')
+            messages.error(self.request, 'You don\'t have order')
             return redirect('/')
 
 
@@ -86,4 +86,58 @@ def remove_from_cart(request, slug):
     else:
         messages.info(request, "Yuo don't have order")
         return redirect('shop:book', slug=slug)
+
+@login_required
+def add_in_cart(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    order_book, created = OrderItem.objects.get_or_create(
+        item=book,
+        user=request.user,
+        ordered=False
+    )
+    order_queryset = Order.objects.filter(user=request.user, ordered=False)
+    if order_queryset.exists():
+        order = order_queryset[0]
+        if order.items.filter(item__slug=book.slug).exists():
+            order_book.quantity += 1
+            order_book.save()
+            messages.info(request, 'This book was again added to your cart')
+        else:
+            order.items.add(order_book)
+            messages.info(request, 'This book was added to your cart')
+    else:
+        order_date = timezone.now()
+        order = Order.objects.create(user=request.user, ordered_date=order_date)
+        order.items.add(order_book)
+        messages.info(request, 'This book was added to your cart')
+    return redirect('shop:cart')
+
+@login_required
+def remove_book_in_cart(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    order_queryset = Order.objects.filter(
+        ordered=False,
+        user=request.user,
+    )
+    if order_queryset.exists():
+        order = order_queryset[0]
+        if order.items.filter(item__slug=book.slug).exists():
+            order_book = OrderItem.objects.filter(
+                item=book,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_book.quantity < 0:
+                order_book.quantity -= 1
+                order_book.save()
+            else:
+                order.items.remove(order_book)
+            messages.info(request, 'This book was delete from your cart')
+            return redirect('shop:cart')
+        else:
+            messages.info(request, 'This book was not in your cart')
+            return redirect('shop:cart')
+    else:
+        messages.info(request, "Yuo don't have order")
+        return redirect('shop:cart')
 
