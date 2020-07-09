af43926952a3ffd2c76e3fa5be9b60aba40d51c7
semen-ksv <ksvsem@gmail.com>
2020-07-09 16:46:54 +0300

    6--count total price in cart

--- shop/models.py
+++ shop/models.py
@@ -46,6 +46,7 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+
 class OrderItem(models.Model):
     item = models.ForeignKey(Book, on_delete=models.CASCADE, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -55,6 +56,9 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f'{self.quantity} of {self.item.title}'
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
 
 
 class Order(models.Model):
@@ -67,4 +71,10 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_total_price(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_total_item_price()
+        return total
 
