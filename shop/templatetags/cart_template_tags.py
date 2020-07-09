5d38392f115181e0515660589d26dc4fcc0ddada
semen-ksv <ksvsem@gmail.com>
2020-07-09 14:36:08 +0300

    5--count books in cart add author at book info

--- shop/templatetags/cart_template_tags.py
+++ shop/templatetags/cart_template_tags.py
@@ -7,7 +7,8 @@ register = template.Library()
 @register.filter
 def cart_item_count(user):
     if user.is_authenticated:
-        qs = Order.objects.filter(user=user, ordered=False)
-        if qs.exists():
-            return qs[0].items.count()
+        books = Order.objects.filter(user=user, ordered=False)
+        if books.exists():
+            print(books[0].items.count())
+            return books[0].items.count()
     return 0
